0e20b4e612471fbbd032d9afb373145ff22d4f54
aikaterna <20862007+aikaterna@users.noreply.github.com>
2021-02-16 14:48:25 -0800

    Handle messages that don't have an embed attrib

--- discord/state.py
+++ discord/state.py
@@ -252,8 +252,11 @@ class ConnectionState:
                 # call state message edit
                 message._handle_call(data['call'])
             elif 'content' not in data:
-                # embed only edit
-                message.embeds = data['embeds']
+                try:
+                    # embed only edit
+                    message.embeds = data['embeds']
+                except KeyError:
+                    pass
             else:
                 message._update(channel=message.channel, **data)
 
